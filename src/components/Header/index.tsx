8b1ea2ff30eccb46118ae712fc4fe1abd3c8036a
Diego <diego_coelho@live.com>
2026-03-05 10:24:31 -0300

    feat: adiciona página de detalhes do produto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
         <div className="relative">
             <header className="fixed top-10 left-0 right-0 z-10 mx-10">
                 <div className='bg-white text-black max-w-[1320] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5'>
-                    <img src={Logo} alt="Logo Syntax Wear" className='w-32 md:w-36' />
+                    <Link to="/"><img src={Logo} alt="Logo Syntax Wear" className='w-32 md:w-36' /></Link>
                     <nav className='hidden md:block'>
                         <ul className='flex gap-10'>
                             <li><a href="#">Masculino</a></li>
