9402cfe65f5929b04c647e81febac4125df929e9
Diego <diego_coelho@live.com>
2026-03-11 10:53:04 -0300

    feat: adiciona armazenamento localstorage

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -13,10 +13,15 @@ export const ShoppingCart = () => {
   return (
     <>
       <button
-        className="cursor-pointer"
+        className="relative cursor-pointer"
         onClick={() => setCartIsOpen(!cartIsOpen)}
       >
         <img src={IconCart} alt="Ícone carrinho de compras" />
+        {cart.length > 0 && (
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+            {cart.length}
+          </span>
+        )}
       </button>
 
       {/* { Overlay } */}
